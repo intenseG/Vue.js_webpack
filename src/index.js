40680a659878e0c8a4ea79977c364b9964a524a7
intenseG <intenseg1515@gmail.com>
2021-07-22 11:33:19 +0900

    リファクタリング（input要素に不具合あり）

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 Vue.component("search-field", {
-  props: ["nameCondition", "gender", "organizationId", "organizations"],
+  props: ["nameCondition", "gender", "organizationId", "organizations", "customers"],
   methods: {
     searchCustomers: async function() {
       const response = await fetch('./resources/customers.json');
@@ -10,7 +10,7 @@ Vue.component("search-field", {
       }
 
       let words = [];
-      if (this.nameCondition != "") {
+      if (this.nameCondition && this.nameCondition != "") {
         this.nameCondition = this.nameCondition.replace(/　/g, " "); //全角スぺースを半角に置換
 
         if (this.nameCondition.includes(" ")) {
@@ -35,13 +35,12 @@ Vue.component("search-field", {
         }
 
         if (isOK) {
-          const organizationName = this.getOrganizationName(data[i].organizationId);
           const customer = {
             id: data[i].id,
             name: data[i].name,
             gender: data[i].gender,
             birthday: data[i].birthday,
-            organization: organizationName
+            organizationId: data[i].organizationId
           };
           this.customers.push(customer);
         }
@@ -54,7 +53,7 @@ Vue.component("search-field", {
       <input type="text" v-model="nameCondition">
       <span class="title">性別</span>
       <label for="unspecified">指定なし</label>
-      <input type="radio" id="unspecified" value="指定なし" checked v-model="gender">
+      <input type="radio" id="unspecified" value="指定なし" v-model="gender" checked>
       <label for="man">男性</label>
       <input type="radio" id="man" value="男" v-model="gender">
       <label for="woman">女性</label>
@@ -74,27 +73,22 @@ Vue.component("customer", {
   props: ["customer", "organizations"],
   methods: {
     getOrganizationName: function() {
-      let organizationName = "";
-      for (let i = 0; i < organizations.length; i++) {
-        if (organizations[i].id == customer.organizationId) {
-          organizationName = organizations[i].name;
-          break;
-        }
-      }
+      const target = this.customer.organizationId;
+      const result = this.organizations.find(function(org) {
+        return org.id === target
+      });
 
-      return organizationName;
+      return result.name;
     }
   },
   template: `
-    <div id="customer">
-      <tr>
-        <td>{{ customer.id }}</td>
-        <td>{{ customer.name }}</td>
-        <td>{{ customer.gender }}</td>
-        <td>{{ customer.birthday }}</td>
-        <td>{{ getOrganizationName() }}</td>
-      </tr>
-    </div>
+    <tr>
+      <td>{{ customer.id }}</td>
+      <td>{{ customer.name }}</td>
+      <td>{{ customer.gender }}</td>
+      <td>{{ customer.birthday }}</td>
+      <td>{{ getOrganizationName() }}</td>
+    </tr>
   `
 });
 
@@ -137,91 +131,23 @@ var app = new Vue({
       const data = await response.json();
 
       this.organizationArray = data;
-
-      // for (let i = 0; i < data.length; i++) {
-      //     const organization = {
-      //       id: data[i].id,
-      //       name: data[i].name
-      //     };
-      //     this.organizationArray.push(organization);
-      // }
     },
     setCustomers: async function() {
       const response = await fetch('./resources/customers.json');
       const data = await response.json();
 
       this.customerArray = data;
-
-      // for (let i = 0; i < data.length; i++) {
-      //   const customer = {
-      //     id: data[i].id,
-      //     name: data[i].name,
-      //     gender: data[i].gender,
-      //     birthday: data[i].birthday,
-      //     organizationId: data[i].organizationId
-      //   };
-      //   this.customerArray.push(customer);
-      // }
     }
-    // getOrganizationName: function(id) {
-    //   let organizationName = "";
-    //   for (let i = 0; i < this.organizations.length; i++) {
-    //     if (this.organizations[i].id == id) {
-    //       organizationName = this.organizations[i].name;
-    //       break;
-    //     }
-    //   }
-
-    //   return organizationName;
-    // },
-    // searchCustomers: async function() {
-    //   const response = await fetch('./resources/customers.json');
-    //   const data = await response.json();
-
-    //   if (this.customers.length > 0) {
-    //     this.customers.splice(0);
-    //   }
-
-    //   let words = [];
-    //   if (this.nameCondition != "") {
-    //     this.nameCondition = this.nameCondition.replace(/　/g, " "); //全角スぺースを半角に置換
-
-    //     if (this.nameCondition.includes(" ")) {
-    //       words = this.nameCondition.split(' ');
-    //     } else {
-    //       words.push(this.nameCondition);
-    //     }
-    //   }
-
-    //   for (let i = 0; i < data.length; i++) {
-    //     if (this.gender != null &&
-    //         data[i].gender != this.gender) continue;
-    //     if (this.organizationId != null &&
-    //         data[i].organizationId != this.organizationId) continue;
-
-    //     let isOK = true;
-    //     for (let j = 0; j < words.length; j++) {
-    //       if (!data[i].name.includes(words[j])) {
-    //         isOK = false;
-    //         break;
-    //       }
-    //     }
-
-    //     if (isOK) {
-    //       const organizationName = this.getOrganizationName(data[i].organizationId);
-    //       const customer = {
-    //         id: data[i].id,
-    //         name: data[i].name,
-    //         gender: data[i].gender,
-    //         birthday: data[i].birthday,
-    //         organization: organizationName
-    //       };
-    //       this.customers.push(customer);
-    //     }
-    //   }
-    // }
   },
   computed: {
+    // _gender: {
+    //   get() {
+    //     return this.gender;
+    //   },
+    //   set(newVal) {
+    //     this.$emit('input', newVal);
+    //   }
+    // },
     organizations: function() {
       return this.organizationArray;
     },
